37718fdd456eaed7d9059823e8b9f9a165e59d7e
Your Name <you@example.com>
2018-12-05 08:20:50 +0300

    Tekstil ve Tasarım Bölümleri eklendi

--- includes/degiskenler.php
+++ includes/degiskenler.php
@@ -1,150 +1,166 @@
 <?php
-   $bolum_ve_programlar=array(
-      array(
-         "ogroup" => "Elektronik ve Otomasyon Bölümü",
-         "vals" => array(
-            array("value" => "Elektronik Teknolojisi Programı"),
-            array("value" => "Elektronik Teknolojisi Programı (İ.Ö.)"),
-            array("value" => "Elektronik Haberleşme Teknolojisi Programı"),
-            array("value" => "Elektronik Haberleşme Teknolojisi Programı (İ.Ö.)"),
-            array("value" => "Kontrol ve Otomasyon Teknolojisi Programı"),
-            array("value" => "Kontrol ve Otomasyon Teknolojisi Programı (İ.Ö.)"),
-            array("value" => "Mekatronik (Uzaktan Eğitim)")
-         )
-      ),
-      array(
-         "ogroup" => "Elektrik ve Enerji Bölümü",
-         "vals" => array(
-           array("value" =>  "Elektrik Programı"),
-           array("value" =>  "Elektrik (Uzaktan Eğitim)")
+$bolum_ve_programlar=array(
+  array(
+    "ogroup" => "Elektronik ve Otomasyon Bölümü",
+    "vals" => array(
+      array("value" => "Elektronik Teknolojisi Programı"),
+      array("value" => "Elektronik Teknolojisi Programı (İ.Ö.)"),
+      array("value" => "Elektronik Haberleşme Teknolojisi Programı"),
+      array("value" => "Elektronik Haberleşme Teknolojisi Programı (İ.Ö.)"),
+      array("value" => "Kontrol ve Otomasyon Teknolojisi Programı"),
+      array("value" => "Kontrol ve Otomasyon Teknolojisi Programı (İ.Ö.)"),
+      array("value" => "Mekatronik (Uzaktan Eğitim)")
+    )
+  ),
+  array(
+    "ogroup" => "Elektrik ve Enerji Bölümü",
+    "vals" => array(
+      array("value" =>  "Elektrik Programı"),
+      array("value" =>  "Elektrik (Uzaktan Eğitim)")
 
-         )
-      ),
-      array(
-         "ogroup" => "Bilgisayar Teknolojileri Bölümü",
-         "vals" => array(
-            array("value" => "Bilgisayar Programcılığı Programı"),
-            array("value" => "Bilgisayar Programcılığı Programı (İ.Ö.)"),
-            array("value" => "İnternet ve Ağ Teknolojileri (Uzaktan Eğitim)")
-         )
-      ),
-      array(
-         "ogroup" => "Makine ve Metal Teknolojileri Bölümü",
-         "vals" => array(
-            array("value" => "Makine Programı"),
-            array("value" => "Makine Programı (İ.Ö.)")
-         )
-      ),
-      array(
-         "ogroup" => "Motorlu Araçlar ve Ulaştırma Teknolojileri Bölümü",
-         "vals" => array(
-            array("value" => "Otomotiv Teknolojisi Programı"),
-            array("value" => "Otomotiv Teknolojisi Programı (İ.Ö.)")
-         )
-      ),
-      array(
-         "ogroup" => "İnşaat Bölümü",
-         "vals" => array(
-            array("value" => "İnşaat Programı"),
-            array("value" => "İnşaat Programı (i.Ö.)")
-         )
-      ),
-      array(
-         "ogroup" => "Mimarlık ve Şehir Planlama Bölümü",
-         "vals" => array(
-            array("value" => "Harita ve Kadastro Programı"),
-            array("value" => "Harita ve Kadastro Programı (İ.Ö.)")
-         )
-      ),
-      array(
-         "ogroup" => "Kimya ve Kimyasal İşleme Teknolojisi Bölümü",
-         "vals" => array(
-            array("value" => "Kimya Teknolojisi Programı"),
-            array("value" => "Kimya Teknolojisi Programı (İ.Ö.)")
-         )
-      )
-   );
-   $staj_donem_ve_tarih=array(
-      array(
-         "ogroup" => "GÜZ EK STAJ",
-         "vals" => array(
-            array(
-               "key" => "guz_0",
-               "value" => "15 Ekim 2018"
-            ),
-            array(
-               "key" => "guz_1",
-               "value" => "05 Kasım 2018"
-            ),
-            array(
-               "key" => "guz_2",
-               "value" => "19 Kasım 2018"
-            )
-         )
-      ),
+    )
+  ),
+  array(
+    "ogroup" => "Bilgisayar Teknolojileri Bölümü",
+    "vals" => array(
+      array("value" => "Bilgisayar Programcılığı Programı"),
+      array("value" => "Bilgisayar Programcılığı Programı (İ.Ö.)"),
+      array("value" => "İnternet ve Ağ Teknolojileri (Uzaktan Eğitim)")
+    )
+  ),
+  array(
+    "ogroup" => "Makine ve Metal Teknolojileri Bölümü",
+    "vals" => array(
+      array("value" => "Makine Programı"),
+      array("value" => "Makine Programı (İ.Ö.)")
+    )
+  ),
+  array(
+    "ogroup" => "Motorlu Araçlar ve Ulaştırma Teknolojileri Bölümü",
+    "vals" => array(
+      array("value" => "Otomotiv Teknolojisi Programı"),
+      array("value" => "Otomotiv Teknolojisi Programı (İ.Ö.)")
+    )
+  ),
+  array(
+    "ogroup" => "İnşaat Bölümü",
+    "vals" => array(
+      array("value" => "İnşaat Programı"),
+      array("value" => "İnşaat Programı (i.Ö.)")
+    )
+  ),
+  array(
+    "ogroup" => "Mimarlık ve Şehir Planlama Bölümü",
+    "vals" => array(
+      array("value" => "Harita ve Kadastro Programı"),
+      array("value" => "Harita ve Kadastro Programı (İ.Ö.)")
+    )
+  ),
+  array(
+    "ogroup" => "Tekstil, Giyim, Ayakkabı ve Deri Bölümü",
+    "vals" => array(
+      array("value" => "Giyim Üretim Teknolojisi Programı"),
+      array("value" => "Giyim Üretim Teknolojisi Programı (İ.Ö.)")
+    )
+  ),
+  array(
+    "ogroup" => "Tasarım Bölümü",
+    "vals" => array(
+      array("value" => "Moda Tasarımı Programı"),
+    )
+  ),
 
+
+
+  array(
+    "ogroup" => "Kimya ve Kimyasal İşleme Teknolojisi Bölümü",
+    "vals" => array(
+      array("value" => "Kimya Teknolojisi Programı"),
+      array("value" => "Kimya Teknolojisi Programı (İ.Ö.)")
+    )
+  )
+);
+$staj_donem_ve_tarih=array(
+  array(
+    "ogroup" => "GÜZ EK STAJ",
+    "vals" => array(
       array(
-         "ogroup" => "BAHAR EK STAJ",
-         "vals" => array(
-            array(
-               "key" => "bahar_0",
-               "value" => "04 Şubat 2019"
-            ),
-            array(
-               "key" => "bahar_1",
-               "value" => "18 Şubat 2019"
-            ),
-            array(
-               "key" => "bahar_2",
-               "value" => "04 Mart 2019"
-            )
-         )
+        "key" => "guz_0",
+        "value" => "15 Ekim 2018"
       ),
       array(
-         "ogroup" => "YAZ STAJI",
-         "vals" => array(
-            array(
-               "key" => "yaz_0",
-               "value" => "24 Haziran 2019"
-            ),
-            array(
-               "key" => "yaz_1",
-               "value" => "22 Temmuz 2019"
-            ),
-            array(
-               "key" => "yaz_2",
-               "value" => "29 Temmuz 2019"
-            )
-         )
-      )
-   );
-   $haftalik_gun = array(
+        "key" => "guz_1",
+        "value" => "05 Kasım 2018"
+      ),
       array(
-         "vals" => array(
-            array(
-               "key" => 5,
-               "value" => "5 Gün"
-            ),
-            array(
-               "key" => 6,
-               "value" => "6 Gün"
-            )
-         )
+        "key" => "guz_2",
+        "value" => "19 Kasım 2018"
       )
-   );
+    )
+  ),
 
-   $ucret = array(
+  array(
+    "ogroup" => "BAHAR EK STAJ",
+    "vals" => array(
       array(
-         "vals" => array(
-            array(
-               "value" => "Evet"
-            ),
-            array(
-               "value" => "Hayır"
-            )
-         )
+        "key" => "bahar_0",
+        "value" => "04 Şubat 2019"
+      ),
+      array(
+        "key" => "bahar_1",
+        "value" => "18 Şubat 2019"
+      ),
+      array(
+        "key" => "bahar_2",
+        "value" => "04 Mart 2019"
       )
-   );
+    )
+  ),
+  array(
+    "ogroup" => "YAZ STAJI",
+    "vals" => array(
+      array(
+        "key" => "yaz_0",
+        "value" => "24 Haziran 2019"
+      ),
+      array(
+        "key" => "yaz_1",
+        "value" => "22 Temmuz 2019"
+      ),
+      array(
+        "key" => "yaz_2",
+        "value" => "29 Temmuz 2019"
+      )
+    )
+  )
+);
+$haftalik_gun = array(
+  array(
+    "vals" => array(
+      array(
+        "key" => 5,
+        "value" => "5 Gün"
+      ),
+      array(
+        "key" => 6,
+        "value" => "6 Gün"
+      )
+    )
+  )
+);
+
+$ucret = array(
+  array(
+    "vals" => array(
+      array(
+        "value" => "Evet"
+      ),
+      array(
+        "value" => "Hayır"
+      )
+    )
+  )
+);
 
 
 
