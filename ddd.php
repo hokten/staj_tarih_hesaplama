727e68d48e487e2f039233de6b1f0bf9449fc501
root <root@instance-1.c.disco-history-206419.internal>
2018-12-05 05:34:13 +0000

    Fazlalıklar silindi

--- ddd.php
+++ ddd.php
@@ -194,11 +194,6 @@ if(array_key_exists("ikinci", $staj_tarihleri)) {
 }
 
 
-$aylar=array(1 => "OCAK", 2 => "ŞUBAT", 3 => "MART", 4 => "NİSAN",
-  5 => "MAYIS", 6 => "HAZİRAN", 7 => "TEMMUZ", 8 => "AĞUSTOS",
-  9 => "EYLÜL", 10 => "EKİM", 11 => "KASIM", 12 => "ARALIK");
-
-$yeni=new TakvimOlustur($tum_staj_baslama, $tum_staj_bitis);
 */
 $ogrenci_alanlari=array(
    array(
